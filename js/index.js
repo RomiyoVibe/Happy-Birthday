cb563b0b49c0a842ab6794cb9ffb626ea6a6c7e0
Nishant9608 <nishantsood9608@gmail.com>
2025-03-14 16:15:33 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||HAPPY|BIRTHDAY|THE|CUTEST|VISHU|❤|#rectangle|');
+      S.UI.simulate('|#countdown 3||HAPPY|BIRTHDAY|TO|THE|CUTEST|PRETTIEST|SWEETEST|MOST|ADORABLE|KINDEST|LOVELIEST|MOST|CHARMING|INCREDIBLY|GORGEOUS|AMAZINGLY|WONDERFUL|AND|ABSOLUTELY|PERFECT|VISHU|❤|#rectangle|');
     }
 
     S.Drawing.loop(function () {
